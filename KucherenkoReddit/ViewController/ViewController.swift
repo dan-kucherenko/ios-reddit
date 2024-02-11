f29ba670f529c096c38a28089e5a2b5e60a1ee9b
dan-kucherenko <daniil.ukraine@gmail.com>
2024-02-11 17:28:04 +0200

    fix sizing of the button

--- KucherenkoReddit/ViewController/ViewController.swift
+++ KucherenkoReddit/ViewController/ViewController.swift
@@ -3,7 +3,6 @@
 //  KucherenkoReddit
 //
 //  Created by Daniil on 10.02.2024.
-//
 
 import UIKit
 
@@ -17,15 +16,13 @@ class ViewController: UIViewController {
     
     @IBAction func onChangeSave(_ sender: UIButton) {
         sender.isSelected.toggle()
+        let config = UIImage.SymbolConfiguration(scale: .medium)
         if sender.isSelected {
-            sender.setImage(UIImage(systemName: "bookmark.fill"), for: .normal)
+            sender.setImage(UIImage(systemName: "bookmark.fill", withConfiguration: config), for: .normal)
+            
+            
         } else {
-            sender.setImage(UIImage(systemName: "bookmark"), for: .normal)
+            sender.setImage(UIImage(systemName: "bookmark", withConfiguration: config), for: .normal)
         }
     }
 }
-
-#Preview {
-    return ViewController()
-}
-
