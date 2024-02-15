7a3d4e43d332ce81275de1c505f8930298d0c4c2
Daniil Kucherenko <daniil.ukraine@gmail.com>
2024-02-15 19:09:09 +0200

    Initial commit for view and view controller

---
+++ KucherenkoReddit/ViewController/PostListViewController.swift
@@ -0,0 +1,12 @@
+//
+//  PostListViewController.swift
+//  KucherenkoReddit
+//
+//  Created by Daniil on 15.02.2024.
+//
+
+import UIKit
+
+class PostListViewController: UITableViewController {
+    
+}
