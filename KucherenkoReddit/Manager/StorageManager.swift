4f3320f855de25c79735f0324a89a185e67a0979
Daniil Kucherenko <daniil.ukraine@gmail.com>
2024-02-27 22:43:26 +0200

    Refactoring

--- KucherenkoReddit/Manager/StorageManager.swift
+++ KucherenkoReddit/Manager/StorageManager.swift
@@ -9,32 +9,30 @@ import Foundation
 
 class StorageManager {
     static let shared = StorageManager()
-    
     private let savedPostsFile = URL.documentsDirectory.appending(path: "savedPosts.json")
     
     private init() {}
     
-    func togglePostSave(_ post: Post) {
-        print(savedPostsFile)
-        var savedPosts = getPosts()
+    func savePost(_ post: Post) {
+        var savedPosts = getSavedPosts()
         if savedPosts.contains(post) {
-            savedPosts.removeAll(where: { post.title == $0.title })
+            savedPosts.removeAll(where: { post.postName == $0.postName })
         } else {
             savedPosts.append(post)
         }
         
-        print(savedPosts.count)
-        guard let encodedPosts = try? JSONEncoder().encode(savedPosts) else { return }
-        try? encodedPosts.write(to: savedPostsFile, options: .atomic)
+        do {
+            let encodedPosts = try JSONEncoder().encode(savedPosts)
+            try encodedPosts.write(to: savedPostsFile, options: .atomic)
+        } catch {
+            print("Error saving post: \(error)")
+        }
     }
     
-    func getPosts() -> [Post] {
-        guard
-            FileManager.default.fileExists(atPath: savedPostsFile.path()),
+    func getSavedPosts() -> [Post] {
+        guard FileManager.default.fileExists(atPath: savedPostsFile.path()),
             let postsData = FileManager.default.contents(atPath: savedPostsFile.path())
-        else {
-            return []
-        }
+        else { return [] }
         return (try? JSONDecoder().decode([Post].self, from: postsData)) ?? []
     }
 }
